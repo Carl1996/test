bbc3dea645db327fe15a463e9c3ce1bdc27fe27e
Carl1996 <30485216+Carl1996@users.noreply.github.com>
2021-07-08 14:17:50 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 #安装Actions需要的依赖库
 sudo -H pip3 install --upgrade setuptools >/dev/null
-#sudo -H pip3 install -r ./run_in_Actions/requirements.txt >/dev/null
+sudo -H pip3 install -r ./requirements.txt >/dev/null
 
 #将secrets映射到配置文件
 #\cp -f ./run_in_Actions/config.json ./config/
